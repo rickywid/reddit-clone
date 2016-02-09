8960429805c863e1b3a761f59c4eb89503ba41c6
Ricky JW <rickywid@hotmail.com>
2016-02-09 16:13:34 -0500

    change root path

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,9 @@ import App from './components/app';
 import TopTopics from './components/toptopics';
 import Comments from './components/comments';
 import SubReddit from './components/subreddit';
-import About from './components/about';
 
 export default(
-	<Route path="/" component={App} >
+	<Route path="/reddit-clone" component={App} >
 		<IndexRoute component={TopTopics} />
 		<Route path="r/:subreddit" component={SubReddit} />
 		<Route path="r/:subreddit/comments/:id/:title" component={Comments} />
